b1a56da776e305463cee4d62192753805e7a359c
AntoineDao <antoine.dao@burohappold.com>
2018-02-25 17:30:56 +0000

    fixed indentation...

--- propertyscraper/accomodation.py
+++ propertyscraper/accomodation.py
@@ -5,7 +5,7 @@ from .address import Address
 from .key_info import KeyInfo
 
 class RentedAccomodation():
-    
+
     def __init__(self, id  = None, href = None):
         self.type = "rented"
         self.id = str(id) if id != None else None
@@ -16,38 +16,38 @@ class RentedAccomodation():
         self.source = None
         self.address = Address()
         self.key_info = KeyInfo()
-        
-    @classmethod    
+
+    @classmethod
     def from_rightmove(cls, id):
         accom = cls(id = id)
-	print("Fetchin property {} from Rightmove...".format(id))
+        print("Fetchin property {} from Rightmove...".format(id))
         accom.href = "http://www.rightmove.co.uk/property-to-rent/property-" + accom.id + ".html"
         accom.soup = BeautifulSoup(requests.get(accom.href).content, "html.parser")
         print("Parsing property...")
 	accom.title = accom.soup.find("h1", {"itemprop": "name"}).getText()
-        
+
         accom.description = re.sub("\s", " ", accom.soup.find("p", {"itemprop":"description"}).getText())
         accom.description = re.sub(" +", " ", accom.description)[1:]
         try:
             accom.tags = [tag.getText() for tag in accom.soup.find("div", {"class": "key-features"}).find_all("li")]
         except:
             pass
-        
+
         accom.address = Address.from_rightmove(accom.soup)
-        
+
         accom.key_info = KeyInfo.from_rightmove(accom.soup)
         accom.source = "rightmove"
-	print("Done!")
+        print("Done!")
         return accom
-    
+
     @classmethod
     def from_zoopla(cls, id):
-	print("Fetching property {} from Zoopla...".format(id))
+        print("Fetching property {} from Zoopla...".format(id))
         accom = cls(id = id)
         accom.href = "https://www.zoopla.co.uk/to-rent/details/" + accom.id
         accom.soup = BeautifulSoup(requests.get(accom.href).content, "html.parser")
         print("Parsing property...")
-	accom.title = accom.soup.find("h2", {"class": "listing-details-h1"}).getText()
+        accom.title = accom.soup.find("h2", {"class": "listing-details-h1"}).getText()
         try:
             accom.description = re.sub("\s", " ", accom.soup.find("div", {"itemprop": "description"}).getText())
         except:
@@ -59,23 +59,23 @@ class RentedAccomodation():
         accom.address = Address.from_zoopla(accom.soup)
         accom.key_info = KeyInfo.from_zoopla(accom.soup)
         accom.source = "zoopla"
-	print("Done!")
+        print("Done!")
         return accom
-    
+
     @classmethod
     def from_gumtree(cls, href):
         accom = cls(href = href)
-	print("Fetching property from Gumtree...")
+        print("Fetching property from Gumtree...")
         accom.soup = BeautifulSoup(requests.get(accom.href).content, "html.parser")
         print("Parsing property...")
-	accom.title = accom.soup.find("h1", {"id": "ad-title"}).getText()
+        accom.title = accom.soup.find("h1", {"id": "ad-title"}).getText()
         accom.description = re.sub("\s", " ", accom.soup.find("p", {"class": "ad-description"}).getText())
         accom.address = Address.from_gumtree(accom.soup)
         accom.key_info = KeyInfo.from_gumtree(accom.soup)
         accom.source = "gumtree"
-	print("Done!")
+        print("Done!")
         return accom
-        
+
     def to_json(self):
         return {
             "title": self.title,
@@ -86,4 +86,3 @@ class RentedAccomodation():
             "key_info": self.key_info.to_json(),
             "tags": self.tags
         }
-        
--- propertyscraper/search.py
+++ propertyscraper/search.py
@@ -4,13 +4,13 @@ from .accomodation import RentedAccomodation
 import re
 
 class RentedSearch():
-    
+
     def __init__(self,link = None):
         self.link = link
         self.soup = BeautifulSoup(requests.get(self.link).content, 'html.parser')
         self.ids = list()
         self.hrefs = list()
-        
+
     @classmethod
     def from_zoopla(cls, link):
         search = cls(link)
@@ -20,19 +20,20 @@ class RentedSearch():
                 search.ids.append(id["data-listing-id"])
             except:
                 pass
-	print("Fetching and parsing latest properties on Zoopla...")
+
+        print("Fetching and parsing latest properties on Zoopla...")
         search.data = [RentedAccomodation.from_zoopla(id) for id in search.ids]
         print("Done with Zoopla!")
 	return search
-            
+
     @classmethod
     def from_rightmove(cls, link):
         search = cls(link)
-	print("Searching latest properties on rightmove...")
+        print("Searching latest properties on rightmove...")
         for id in search.soup.find_all("a", {"class": "propertyCard-anchor"}):
             id = re.sub("prop","",id["id"])
             search.ids.append(id) if len(id) == 8 else None
-	print("Fetching and parsing latest properties on rightmove...")
+        print("Fetching and parsing latest properties on rightmove...")
         search.data = [RentedAccomodation.from_rightmove(id) for id in search.ids]
         print("Done with rightmove!")
 	return search
@@ -40,13 +41,13 @@ class RentedSearch():
     @classmethod
     def from_gumtree(cls, link):
         search = cls(link)
-	print("Searching latest properties on gumtree...")
+        print("Searching latest properties on gumtree...")
         for item in search.soup.find_all("li", {"class": "natural"}):
             href = "https://www.gumtree.com" + item.find("a",{"class":"listing-link"})["href"]
             id = href.split("/")[-1]
             search.hrefs.append(href)
             search.ids.append(id)
-	print("Fetching and parsing latest properties on gumtree...")
+        print("Fetching and parsing latest properties on gumtree...")
         search.data = [RentedAccomodation.from_gumtree(href) for href in search.hrefs]
-	print("Done with gumtree!")
-	return search
+        print("Done with gumtree!")
+        return search
